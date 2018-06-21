72b468ccf7d1c7846824aad2dea612e3625149b4
baobaojianqiang <16230091@qq.com>
2018-06-21 14:33:46 +0800

    1v

---
+++ app/src/main/java/jianqiang/com/hook1/HookHelper.java
@@ -0,0 +1,18 @@
+package jianqiang.com.hook1;
+
+import android.os.Handler;
+
+public class HookHelper {
+
+    public static void attachBaseContext() throws Exception {
+
+        // 先获取到当前的ActivityThread对象
+        Object currentActivityThread = RefInvoke.getStaticFieldObject("android.app.ActivityThread", "sCurrentActivityThread");
+
+        // 由于ActivityThread一个进程只有一个,我们获取这个对象的mH
+        Handler mH = (Handler) RefInvoke.getFieldObject(currentActivityThread, "mH");
+
+        //把Handler的mCallback字段，替换为new MockClass2(mH)
+        RefInvoke.setFieldObject(Handler.class, mH, "mCallback", new MockClass2(mH));
+    }
+}
---
+++ app/src/main/java/jianqiang/com/hook1/MockClass2.java
@@ -0,0 +1,38 @@
+package jianqiang.com.hook1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class MockClass2 implements Handler.Callback {
+
+    Handler mBase;
+
+    public MockClass2(Handler base) {
+        mBase = base;
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+
+        switch (msg.what) {
+            // ActivityThread里面 "LAUNCH_ACTIVITY" 这个字段的值是100
+            // 本来使用反射的方式获取最好, 这里为了简便直接使用硬编码
+            case 100:
+                handleLaunchActivity(msg);
+                break;
+        }
+
+        mBase.handleMessage(msg);
+        return true;
+    }
+
+    private void handleLaunchActivity(Message msg) {
+        // 这里简单起见,直接取出TargetActivity;
+
+        Object obj = msg.obj;
+
+        Log.d("baobao", obj.toString());
+    }
+}
